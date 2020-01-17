531cb0068839134d696aa771b3af80f836b97c17
Aisha Blake <aisha.g.blake@gmail.com>
2020-01-17 00:05:41 -0500

    Sample solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
   title = 'http-fun';
+  data;
+
+  ngOnInit() {
+    this.apiService.getData().subscribe((data) => this.data = { ...data });
+  }
 }
